609a527045c15b95a9240d38d199c36288c8b438
arpans16 <arpan.arko@gmail.com>
2022-09-20 19:01:38 -0400

    New tutorials folder with CEM case

---
+++ tutorials/mhd2Dproblem/solvers/cemFluid2Foam/cemFluid2Foam.C
@@ -0,0 +1,100 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2011-2018 OpenFOAM Foundation
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    arcmhdFoam
+
+Group
+    grpElectroMagneticsSolvers
+
+Description
+    Solver for magnetohydrodynamics (MHD): incompressible, laminar flow of a
+    conducting fluid under the influence of a magnetic field.
+
+    An applied magnetic field H acts as a driving force,
+    at present boundary conditions cannot be set via the
+    electric field E or current density J. The fluid viscosity nu,
+    conductivity sigma and permeability mu are read in as uniform
+    constants.
+
+    A fictitous magnetic flux pressure pH is introduced in order to
+    compensate for discretisation errors and create a magnetic face flux
+    field which is divergence free as required by Maxwell's equations.
+
+    However, in this formulation discretisation error prevents the normal
+    stresses in UB from cancelling with those from BU, but it is unknown
+    whether this is a serious error.  A correction could be introduced
+    whereby the normal stresses in the discretised BU term are replaced
+    by those from the UB term, but this would violate the boundedness
+    constraint presently observed in the present numerics which
+    guarantees div(U) and div(H) are zero.
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "pisoControl.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    argList::addNote
+    (
+        "Solver for magnetohydrodynamics (MHD):"
+        " incompressible, laminar flow of a conducting fluid"
+        " under the influence of a magnetic field."
+    );
+
+    #include "postProcess.H"
+
+    #include "addCheckCaseOptions.H"
+    #include "setRootCaseLists.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+    #include "createControl.H"
+    #include "createFields.H"
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Info<< nl << "Starting time loop" << endl;
+
+    while (runTime.loop())
+    {
+        Info<< "Iteration = " << runTime.timeName() << nl << endl;
+
+	solve(fvm::laplacian(phiE) == fvc::div(U ^ B));
+	phiE.correctBoundaryConditions();
+
+        runTime.write();
+        runTime.printExecutionTime(Info);
+    }
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
